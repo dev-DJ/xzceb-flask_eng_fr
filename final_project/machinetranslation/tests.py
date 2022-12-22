e11fb7dbbf90e7001598d4e13245e30e504eb0c9
your_github_username <youremail@domain.com>
2022-12-22 04:58:52 -0500

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,16 +1,18 @@
 import unittest
+from translator import frenchToEnglish, englishToFrench
 
-from translator import englishToFrench, frenchToEnglish
+class TestTranslator(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
+        self.assertEqual(frenchToEnglish(None),None)
+        self.assertEqual(frenchToEnglish(''),'')
+        self.assertNotEqual(frenchToEnglish('Rouge'), 'Green')
 
-class TestEnglishToFrench(unittest.TestCase): 
-    def test1(self): 
-        self.assertEqual(englishToFrench(""), "")
-        self.assertEqual(englishToFrench("Hello"), "Bonjour")
-             
+    def test_2(self):
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        self.assertEqual(englishToFrench(None),None)
+        self.assertEqual(englishToFrench(''),'')
+        self.assertNotEqual(englishToFrench('Green'), 'Rouge')
 
-class TestFrenchToEnglish(unittest.TestCase): 
-    def test1(self): 
-        self.assertEqual(frenchToEnglish(""), "")
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello")
-        
-unittest.main()
+if __name__=='__main__':
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,39 +1,42 @@
-"""Module providingFunction for EN-FR and FR-EN translation"""
-
+'''Module providing ability to run python script'''
 import os
-import json
+#import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+#apikey = os.environ['apikey']
+#url = os.environ['url']
+
+apikey='_Arm80SfZvT9dW1AVKAg0j1F37IndfACKRGIODdEYxgu'
+url='https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/562a2b95-efac-4af5-831a-7811241b491c'
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
+    version='2018-05-01', 
     authenticator=authenticator
 )
 
 language_translator.set_service_url(url)
-#language_translator.set_disable_ssl_verification(True)
 
+def englishToFrench(englishText):
+    '''
+    Translate english text to french. Return french text
+    '''
+    if englishText =='' or englishText is None:
+        return ''
+    translation=language_translator.translate(text=englishText,model_id='en-fr').get_result()
+    frenchText=translation['translations'][0]['translation']
+    return frenchText
 
-def englishToFrench(english_text):
-    """EN-FR Translation method"""
-    language_translator.set_service_url(url)
-    french_text=language_translator.translate(
-    text= english_text,
-    model_id='en-fr').get_result()
-    return french_text
-
-
-def frenchToEnglish(french_text):
-    """FR-EN Translation method"""
-    language_translator.set_service_url(url)
-    english_text=language_translator.translate(
-    text= french_text,
-    model_id='fr-en').get_result()
-    return english_text
+def frenchToEnglish(frenchText):
+    '''
+    Translate french text to english. Return french text
+    '''
+    if frenchText =='' or frenchText is None:
+        return ''
+    translation=language_translator.translate(text=frenchText,model_id='fr-en').get_result()
+    englishText=translation['translations'][0]['translation']
+    return englishText
